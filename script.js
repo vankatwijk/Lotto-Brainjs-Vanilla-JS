808fd1ced55468baa6c9967f06a8b078b287e971
vankatwijk <hendrikus.hpvk@gmail.com>
2020-12-03 11:48:46 +0100

    better layout and diagram added

--- script.js
+++ script.js
@@ -1,113 +1,272 @@
-const net = new brain.NeuralNetwork()
-// adding more hidden layers makes the output better
-// const net = new brain.NeuralNetwork({
-//     hiddenLayers:[4, 5,6]
-// })
 
+//https://stackoverflow.com/questions/39927452/recursively-print-all-permutations-of-a-string-javascript
+function permut(string) {
+    if (string.length < 2) return string; // This is our break condition
 
-const data = [{
-    "input": {
-        "r": 0,
-        "g": 0,
-        "b": 0
-    },
-    "output": [1]
-}, {
-    "input": {
-        "r": 1,
-        "g": 1,
-        "b": 1
-    },
-    "output": [0]
-}, {
-    "input": {
-        "r": 0.572953898038113,
-        "g": 0.17984830956413367,
-        "b": 0.9205524134715568
-    },
-    "output": [1]
-}, {
-    "input": {
-        "r": 0.5867333801426258,
-        "g": 0.40788591930767937,
-        "b": 0.5605131914538457
-    },
-    "output": [1]
-}, {
-    "input": {
-        "r": 0.08952283206685152,
-        "g": 0.10733121769862053,
-        "b": 0.8658706919401136
-    },
-    "output": [1]
-}, {
-    "input": {
-        "r": 0.7723219410495117,
-        "g": 0.6321997315545567,
-        "b": 0.6066527362792624
-    },
-    "output": [1]
-},{
-    "input": {
-        "r": 0.28882545725910314,
-        "g": 0.5275180797439396,
-        "b": 0.5488851379936934
-    },
-    "output": [1]
-}]
+    var permutations = []; // This array will hold our permutations
 
+    for (var i=0; i<string.length; i++) {
+        var char = string[i];
 
+        // Cause we don't want any duplicates:
+        if (string.indexOf(char) != i) // if char was used already
+            continue;           // skip it this time
 
-//x or operations
-net.train(data)
+        var remainingString = string.slice(0,i) + string.slice(i+1,string.length); //Note: you can concat Strings via '+' in JS
 
-// const diagram = document.getElementById('diagram')
-// diagram.innerHTML = brain.utilities.toSVG(net)
+        for (var subPermutation of permut(remainingString))
+            permutations.push(char + subPermutation)
 
-//test if it works with an input
-//console.log(net.run({r:1, g:1,b:0}))
-
-const colorE1 = document.getElementById('color')
-const guessE1 = document.getElementById('guess')
-const whiteButton = document.getElementById('white-button')
-const blackButton = document.getElementById('black-button')
-const printButton = document.getElementById('print-button')
-let color
-setRandomColor()
-
-whiteButton.addEventListener('click', () => {
-    chooseColor(1)
-})
-
-blackButton.addEventListener('click', () => {
-    chooseColor(0)
-})
-
-printButton.addEventListener('click', print)
-
-function chooseColor(value) {
-    data.push({
-        input: color,
-        output: [value]
-    })
-    setRandomColor()
-
-}
-
-function print() {
-    console.log(JSON.stringify(data))
-}
-
-function setRandomColor() {
-    color = {
-        r: Math.random(),
-        g: Math.random(),
-        b: Math.random()
     }
-    const guess = net.run(color)[0]
+    return permutations;
+}
 
-    guessE1.style.color = guess > .5 ? '#FFF' : '#000'
+function clean(s){
+    r = s.replace(/\D/g,' ');
+    r = r.replace(/\s\s+/g, ' ');
+    return r;
+}
+function zpadlen(n,len){
+    return ("00000"+n).slice(-len);
+}
+function zpad(n){
+    return ("0000"+n).slice(-4);
+}
+function zpad2(n){
+    return ("0000"+n).slice(-2);
+}
+function zpad3(n){
+    return ("0000"+n).slice(-3);
+}
+function zpad4(n){
+    return ("0000"+n).slice(-4);
+}
+function sumstr(s){
+    r = 0;
+    for (i=0;i<s.length;i++){
+        r += parseInt(s[i]);
+    }
+    return r;
+}
+function rootstr(s){
+    n = parseInt(s);
+    while (n > 9){
+        n = sumstr(n+"") + "";
+        n = parseInt(n);
+    }
+    return n;
+}
+function tablerow(row){
+    r = "<tr>"
+    for (i=0;i<row.length;i++){
+        r += "<td align='center'>"+row[i]+"</td>"
+    }
+    r += "</tr>";
+    return r;
+}
+function addstr(s1,s2){
+    r = "";
+    for (i=0;i<s1.length;i++){
+        r+= "" + ((parseInt(s1[i]) + parseInt(s2[i]))%10);
+    }
+    return r;
+}
+function subarr(a1,a2){
+    r = [];
+    for (i=0;i<a1.length;i++){
+        r.push(parseInt(a1[i]) - parseInt(a2[i]));
+    }
+    return r;
+}
+function addarr(a1, a2){
+    r = [];
+    for (i=0;i<a1.length;i++){
+        r.push(parseInt(a1[i]) + parseInt(a2[i]));
+    }
+    return r; 
+}
+function bracket_if_match(str,n){
+    r = [];
+    for (var i=0;i<str.length;i++){
+        if (parseInt(str[i]) == parseInt(n)){
+        r.push("(" + str[i] + ")");
+        }else{
+        r.push(str[i]);
+        }
+    }
+    return r;
+}
 
-    colorE1.style.backgroundColor =
-        `rgba(${color.r*255},${color.g*255},${color.b*255})`
+function mirror(n){
+    n = "" + n;
+    r = "";
+    for (var i=0;i<n.length;i++){
+        r += ((parseInt(n[i])+5)%10) + "";
+    }
+    return r;
+}
+function mirrorarr(ar){
+    var r = [];
+    for (var i=0;i<ar.length;i++){
+        r.push(mirror(ar[i]));
+    }
+    return r;
+}
+
+function intarr(ar){
+    var re = [];
+    for (var i=0;i<ar.length;i++){
+        re.push(parseInt(ar[i]));
+    }
+    return re;
+}
+function lotmat(ar){
+    re = 0;
+    for (i=0;i<ar.length;i++){
+        re += parseInt(ar[i]);
+    }
+    re = re % 10;
+    return re;
+}
+
+
+var net = 0;
+var tD = 0;
+var lastDraw = 0;
+var lastResult = 0;
+
+
+function createnetwork(){
+    hidlayers = eval("["+document.getElementById("layers").value+"]");
+    net = new brain.NeuralNetwork({ hiddenLayers: hidlayers});
+    outputt = "Neural Net created with Hidden Layers Shape ["+ hidlayers.join(",") +"]<br/>";
+    document.getElementById('output').innerHTML = outputt;
+
+}
+function reverseInputLines(){
+    input = document.getElementById('input').value;
+    input = input.split("\n");
+    input = input.reverse();
+    input = input.join("\n");
+    document.getElementById('input').value = input;
+}  
+
+function highestProb(result,print=1){
+    orderlst = [];
+    for (var i=0;i<result.length;i++){
+        orderlst.push([i+1,result[i]]);
+    }
+    orderlst = orderlst.sort(function(a,b){return b[1]-a[1]});
+    lst = [];
+    if (print==1){
+        document.getElementById('output').innerHTML += "Highest probability is: " + orderlst[0][1] + "<br/>";
+        document.getElementById('output').innerHTML += "Lowest probability is: " + orderlst[orderlst.length-1][1] + "<br/>";
+    }
+    for (var i=0;i<orderlst.length;i++){
+        lst.push(orderlst[i][0]);
+    }
+    return lst;
+}
+function trainnetwork(){
+    //for (var i=0;i<1;i++){
+    stats = net.train(tD);
+    document.getElementById('output').innerHTML = "Error:" + stats["error"] + " Iterations: " + stats['iterations'];
+    document.getElementById('output').innerHTML += "<br/>Trained.<br/>Run with last Draw result: " + lastDraw.join(", ") +"<br/>";
+    const diagram = document.getElementById('networkd')
+    diagram.innerHTML = brain.utilities.toSVG(net)
+    runthrough();
+}
+
+function runthrough(){
+    result = net.run(lastResult);
+    ordlst = highestProb(result);
+    numbersToPlay = ordlst.slice(0,7);
+    document.getElementById('output').innerHTML += "From most likely to least likely: " + ordlst.join(', ') + "<br/>";
+    document.getElementById('output').innerHTML += "Numbers to play: <b>" + numbersToPlay.join(' ') + "</b><br/>";
+    document.getElementById('output').innerHTML += "Here are 10 sets ran in series (which could be played as well including the above set):<br/>";
+    nextResult = lastResult;
+    balls = ordlst.length;
+    numbers = numbersToPlay.length;
+    for (var i=0;i<5;i++){
+        result = net.run(nextResult);
+        ordlst = highestProb(result,print=0);
+        thisSet = ordlst.slice(0,numbers);
+        document.getElementById('output').innerHTML += "<b>"+thisSet.join(" ")+"</b><br/>";
+        nextResult = tD_Ones(balls,thisSet);
+    }
+
+}
+function trainnetwork100(){
+    for (var i=0;i<100;i++){
+        net.train(tD);
+
+    }
+    document.getElementById('output').innerHTML = "<br/>Trained 100x.<br/>Run with last Draw result: " + lastDraw.join(", ") +"<br/>";
+    runthrough();
+}
+
+function trainnetwork1000(){
+    for (var i=0;i<1000;i++){
+        net.train(tD);
+
+    }
+    document.getElementById('output').innerHTML = "<br/>Trained 1000x.<br/>Run with last Draw result: " + lastDraw.join(", ") +"<br/>";
+    runthrough();
+}
+
+function readtrainingdata(){
+    run();
+}
+
+
+function tD_Ones(size,row){
+    var res = [];
+    for (var i=0;i<size;i++){
+        res.push(0);
+    }
+    for (var i=0;i<row.length;i++){
+        curindex = parseInt(row[i]) - 1;
+        res[curindex] = 1;
+    }
+    return res;
+}
+
+function run(){
+    outputt = "";
+    //check these numbers in 'check' element
+    //lastdraw =  document.getElementById('draw').value;
+    check = document.getElementById('input').value;
+    check = clean(check);
+    checks = check.split(" ");
+    var filtered = checks.filter(function (el) {//filter out empty
+        return el != "";
+    });
+    checks = filtered;
+
+
+    inputs = checks;
+    //len = inputs[0].length; //Pick-3/Pick-4 indicator based on len.
+    len = 7;
+
+    // put them in groups of 6 for each item
+    finputs = [];
+    for (var i=0;i<(Math.floor(inputs.length/8));i++){
+        input = inputs.slice(i*8,i*8+8);
+        finputs.push(input);
+    }
+    inputs = finputs;
+
+    inputs = inputs.reverse(); //reverse so that it's from oldest to newest
+    balls = 50;
+
+    tD = [];
+    for (var i=0;i<inputs.length-1;i++){
+        tDin = tD_Ones(balls,inputs[i].slice(0,len));
+        tDout = tD_Ones(balls,inputs[i+1].slice(0,len));
+        tD.push({input:tDin,output:tDout});
+    }
+    lastDraw = inputs[inputs.length-1];
+    lastResult = tD_Ones(balls,lastDraw.slice(0,len));
+    outputt+="Read training data!";
+    document.getElementById('output').innerHTML = outputt;
 }
